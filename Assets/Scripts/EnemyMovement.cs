ec59e35cd18ac0ce8bc2ad6340607cd77f8ab314
SedenCanpolat <sedencanpolat@gmail.com>
2025-03-07 00:26:22 +0300

    look around done :D

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -6,19 +6,33 @@ public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
     [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _maxSeeAngle;
     private FieldOfView _fieldOfView;
     private RaycastHit _raycastHit;
+    private Quaternion _startRotation;
+    private float _directionIndicator = 1f; 
+    private float _rotation;
     
     void Start()
     {
         _fieldOfView = GetComponent<FieldOfView>();
-        
+        _startRotation = transform.rotation;
     }
 
     void Update()
     {
+        _rotation += _directionIndicator * _rotationSpeed * Time.deltaTime;
+        
+        if (_rotation >= _maxSeeAngle || _rotation <= -_maxSeeAngle)
+        {
+            _directionIndicator *= -1;
+        }
 
-        if(_fieldOfView.IsInView(out _raycastHit)){
+        transform.rotation = _startRotation * Quaternion.Euler(0, _rotation, 0);
+        
+        if (_fieldOfView.IsInView(out _raycastHit))
+        {
             transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed * Time.deltaTime);
         }
     }
