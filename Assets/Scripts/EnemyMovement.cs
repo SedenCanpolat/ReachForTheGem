e303f6208fe2dd6a22c6906c6e0cbb484fc1230b
SedenCanpolat <sedencanpolat@gmail.com>
2025-03-08 18:32:35 +0300

    patrol finalized

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,32 +8,68 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _rotationSpeed;
     [SerializeField] private float _maxSeeAngle;
+    [SerializeField] private float _walkDistance;
+    [SerializeField] private bool isRight = false;
     private FieldOfView _fieldOfView;
     private RaycastHit _raycastHit;
     private Quaternion _startRotation;
     private float _directionIndicator = 1f; 
     private float _rotation;
+    private CharacterController _controller;
+    private float _movedDistance = 0f;
     
     void Start()
     {
         _fieldOfView = GetComponent<FieldOfView>();
         _startRotation = transform.rotation;
+        _controller = GetComponent<CharacterController>();
+        
     }
 
     void Update()
     {
-        _rotation += _directionIndicator * _rotationSpeed * Time.deltaTime;
         
-        if (_rotation >= _maxSeeAngle || _rotation <= -_maxSeeAngle)
-        {
-            _directionIndicator *= -1;
-        }
-
-        transform.rotation = _startRotation * Quaternion.Euler(0, _rotation, 0);
+        _patrol();
         
         if (_fieldOfView.IsInView(out _raycastHit))
         {
             transform.position = Vector3.MoveTowards(transform.position, _player.transform.position, _speed * Time.deltaTime);
         }
+    }
+
+    private void _patrol(){
+        Vector3 moveDirection = isRight ? Vector3.right : Vector3.forward;
+        Vector3 moveAmount = moveDirection * _speed * Time.deltaTime;
+        _controller.Move(moveAmount);
+        _movedDistance += moveAmount.magnitude;
+        if (_movedDistance >= _walkDistance)
+        {
+            StartCoroutine(_lookAround(10));
+            _movedDistance = 0f;
+        }
+        
+        transform.rotation = Quaternion.LookRotation(moveAmount.normalized);
+    }
+
+    private IEnumerator _lookAround(float time){
+        float actualSpeed = _speed;
+        _speed *= 0;
+        float elapsedTime = 0f;
+        while(elapsedTime < time){
+            _rotation += _directionIndicator * _rotationSpeed * Time.deltaTime;
+        
+            if (_rotation >= _maxSeeAngle || _rotation <= -_maxSeeAngle)
+            {
+                _directionIndicator *= -1;
+            }
+
+            transform.rotation = _startRotation * Quaternion.Euler(0, _rotation, 0);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+        _speed = actualSpeed;
+        _speed *= -1;
+        
     }
 }
